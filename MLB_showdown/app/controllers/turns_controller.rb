ccc592d42e75f72a9ec9d7ff0f337b2efd6cdb00
Skylar Shibayama <skylar.shibayama@yale.edu>
2016-06-27 13:51:32 -0400

    please work

--- MLB_showdown/app/controllers/turns_controller.rb
+++ MLB_showdown/app/controllers/turns_controller.rb
@@ -13,12 +13,12 @@ class TurnsController < ApplicationController
     a.pitch_decision = params['pitch_decision']
     a.roll_1 = params['roll_1']
     a.roll_2 = params['roll_2']
-      if a.roll_1.blank?
-        a.roll_1 = 1+rand(20)
-      end
-      if a.roll_2.blank?
-        a.roll_2 = 1+rand(20)
-      end
+    if a.roll_1.blank?
+      a.roll_1 = 1+rand(20)
+    end
+    if a.roll_2.blank?
+      a.roll_2 = 1+rand(20)
+    end
     a.outs = params['outs']
     a.home_runs = params['home_runs']
     a.visitor_runs = params['visitor_runs']
@@ -89,44 +89,200 @@ class TurnsController < ApplicationController
     @batter = T1batter.find_by id: a.batter_id
     @pitcher = T2pitcher.find_by id: a.pitcher_id
     a.save
-  end
 
-  def edit
-    id = params[:id]
-    @turn = Turn.find(id)
-  end
-
-  def show
-    @current_game_id = params['game_id']
-    id = params[:id]
-    @turn = Turn.find(id)
-
-    # base state image
-    i = @turn.ending_base_state_id || 1
-    @base_state_image = BaseState.find(i).image
-
-
-
-
-
-  end
-
-  def update
-    id = params['id']
-    turn_hash = params.delete('turn')
-    @turn = Turn.find(id)
-    @turn.game_id = turn_hash['game_id']
-
-
-    if @turn.save
-      redirect_to turn_path(@turn.id)
+    if @pitcher.PU == 0
+      @showSO = "-"
+    elsif @pitcher.PU == 1
+      @showPU = "1"
+    elsif @pitcher.PU > 1
+      @showPU = "1-" + @pitcher.PU.to_s
     end
-  end
+    @cum = @pitcher.PU
 
-  def destroy
-    id = params['id']
-    Turn.delete(id)
-    flash[:notice] = "You've deleted turn #{id}"
-    redirect_to turn_path
+    if @pitcher.SO == 0
+      @showSO = "-"
+    elsif @pitcher.SO== 1
+      @showSO = @cum + 1
+    elsif @pitcher.SO > 1
+      @showSO = (1 + @cum).to_s + "-" + (@cum + @pitcher.SO).to_s
+    end
+    @cum = @cum + @pitcher.SO
+
+    if @pitcher.GB == 0
+      @showGB = "-"
+    elsif @pitcher.GB== 1
+      @showGB = @cum + 1
+    elsif @pitcher.GB > 1
+      @showGB = (1 + @cum).to_s + "-" + (@cum + @pitcher.GB).to_s
+    end
+    @cum = @cum + @pitcher.GB
+
+    if @pitcher.FB == 0
+      @showFB = "-"
+    elsif @pitcher.FB== 1
+      @showFB = @cum + 1
+    elsif @pitcher.FB > 1
+      @showFB = (1 + @cum).to_s + "-" + (@cum + @pitcher.FB).to_s
+    end
+    @cum = @cum + @pitcher.FB
+
+    if @pitcher.BB == 0
+      @showBB = "-"
+    elsif @pitcher.BB== 1
+      @showBB = @cum + 1
+    elsif @pitcher.BB > 1
+      @showBB = (1 + @cum).to_s + "-" + (@cum + @pitcher.BB).to_s
+    end
+    @cum = @cum + @pitcher.BB
+
+    if @pitcher._1B == 0
+      @show1B = "-"
+    elsif @pitcher._1B== 1
+      @show1B = @cum + 1
+    elsif @pitcher._1B > 1
+      @show1B = (1 + @cum).to_s + "-" + (@cum + @pitcher._1B).to_s
+    end
+    @cum = @cum + @pitcher._1B
+
+    if @pitcher._2B == 0
+      @show2B = "-"
+    elsif @pitcher._2B== 1
+      @show2B = @cum + 1
+    elsif @pitcher._2B > 1
+      @show2B = (1 + @cum).to_s + "-" + (@cum + @pitcher._2B).to_s
+    end
+    @cum = @cum + @pitcher._2B
+
+    if @pitcher._HR == 0
+      @showHR = "-"
+    elsif @pitcher._HR== 1
+      @showHR = @cum + 1
+    elsif @pitcher._HR > 1
+      @showHR = (1 + @cum).to_s + "-" + (@cum + @pitcher._HR).to_s
+    end
+    @cum = @cum + @pitcher._HR
+
+
+
+    if @batter.SO == 0
+      @showSO = "-"
+    elsif @batter.SO == 1
+      @showSO = "1"
+    elsif @batter.SO > 1
+      @showSO = "1-" + @batter.SO.to_s
+    end
+    @cum = @batter.SO
+
+    if @batter.GB == 0
+      @showGB = "-"
+    elsif @batter.GB== 1
+      @showGB = @cum + 1
+    elsif @batter.GB > 1
+      @showGB = (1 + @cum).to_s + "-" + (@cum + @batter.GB).to_s
+    end
+    @cum = @cum + @batter.GB
+
+    if @batter.FB == 0
+      @showFB = "-"
+    elsif @batter.FB== 1
+      @showFB = @cum + 1
+    elsif @batter.FB > 1
+      @showFB = (1 + @cum).to_s + "-" + (@cum + @batter.FB).to_s
+    end
+    @cum = @cum + @batter.FB
+
+    if @batter.BB == 0
+      @showBB = "-"
+    elsif @batter.BB== 1
+      @showBB = @cum + 1
+    elsif @batter.BB > 1
+      @showBB = (1 + @cum).to_s + "-" + (@cum + @batter.BB).to_s
+    end
+    @cum = @cum + @batter.BB
+
+    if @batter._1B == 0
+      @show1B = "-"
+    elsif @batter._1B== 1
+      @show1B = @cum + 1
+    elsif @batter._1B > 1
+      @show1B = (1 + @cum).to_s + "-" + (@cum + @batter._1B).to_s
+    end
+    @cum = @cum + @batter._1B
+
+    if @batter._1Bplus == 0
+      @show1Bplus = "-"
+    elsif @batter._1Bplus== 1
+      @show1Bplus = @cum + 1
+    elsif @batter._1Bplus > 1
+      @show1Bplus = (1 + @cum).to_s + "-" + (@cum + @batter._1Bplus).to_s
+    end
+    @cum = @cum + @batter._1Bplus
+
+    if @batter._2B == 0
+      @show2B = "-"
+    elsif @batter._2B== 1
+      @show2B = @cum + 1
+    elsif @batter._2B > 1
+      @show2B = (1 + @cum).to_s + "-" + (@cum + @batter._2B).to_s
+    end
+    @cum = @cum + @batter._2B
+
+    if @batter._3B == 0
+      @show3B = "-"
+    elsif @batter._3B== 1
+      @show3B = @cum + 1
+    elsif @batter._3B > 1
+      @show3B = (1 + @cum).to_s + "-" + (@cum + @batter._3B).to_s
+    end
+    @cum = @cum + @batter._3B
+
+    if @batter._HR == 0
+      @showHR = "-"
+    elsif @batter._HR== 1
+      @showHR = @cum + 1
+    elsif @batter._HR > 1
+      @showHR = (1 + @cum).to_s + "-" + (@cum + @batter._HR).to_s
+    end
+    @cum = @cum + @batter._HR
+
+
+    def edit
+      id = params[:id]
+      @turn = Turn.find(id)
+    end
+
+    def show
+      @current_game_id = params['game_id']
+      id = params[:id]
+      @turn = Turn.find(id)
+
+      # base state image
+      i = @turn.ending_base_state_id || 1
+      @base_state_image = BaseState.find(i).image
+
+
+
+
+
+    end
+
+    def update
+      id = params['id']
+      turn_hash = params.delete('turn')
+      @turn = Turn.find(id)
+      @turn.game_id = turn_hash['game_id']
+
+
+      if @turn.save
+        redirect_to turn_path(@turn.id)
+      end
+    end
+
+    def destroy
+      id = params['id']
+      Turn.delete(id)
+      flash[:notice] = "You've deleted turn #{id}"
+      redirect_to turn_path
+    end
   end
 end
