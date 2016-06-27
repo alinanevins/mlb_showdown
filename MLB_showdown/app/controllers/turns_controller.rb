31690cfab0cbfa8590f30da9914a3c6708ae02de
Skylar Shibayama <skylar.shibayama@yale.edu>
2016-06-27 12:34:39 -0400

    added a bunch of stuff...scoreboard, gameplay fixes

--- MLB_showdown/app/controllers/turns_controller.rb
+++ MLB_showdown/app/controllers/turns_controller.rb
@@ -4,6 +4,7 @@ class TurnsController < ApplicationController
   end
 
   def create
+
     a = Turn.new
     a.inning = params['inning']
     a.at_bat = params['at_bat']
@@ -12,9 +13,19 @@ class TurnsController < ApplicationController
     a.pitch_decision = params['pitch_decision']
     a.roll_1 = params['roll_1']
     a.roll_2 = params['roll_2']
+      if a.roll_1.blank?
+        a.roll_1 = 1+rand(20)
+      end
+      if a.roll_2.blank?
+        a.roll_2 = 1+rand(20)
+      end
+    a.outs = params['outs']
+    a.home_runs = params['home_runs']
+    a.visitor_runs = params['visitor_runs']
+    @batter = T1batter.find_by id: Turn.last.batter_id
+    @pitcher = T2pitcher.find_by id: Turn.last.pitcher_id
     a.game_id = 1
     params.delete('turn')
-
     @batter = T1batter.find_by id: 3
     @pitcher = T2pitcher.first
 
@@ -23,6 +34,12 @@ class TurnsController < ApplicationController
 
     c = ResultService.new(a.advantage, a.roll_2, @pitcher, @batter)
     a.result = c.calculate_result
+    d = EndingBaseStateService.new(a.base_state_id, a.result, a.outs, a.home_runs, a.visitor_runs, a.at_bat, 0)
+    a.ending_base_state_id = d.calculate_ending_base_state
+    a.outs = d.outs
+    a.home_runs = d.home_runs
+    a.visitor_runs = d.visitor_runs
+
 
     a.result = c.calculate_result
     d = EndingBaseStateService.new(a.base_state_id, a.result, a.outs, a.home_runs, a.visitor_runs, a.at_bat, 0)
@@ -66,6 +83,12 @@ class TurnsController < ApplicationController
     @base_state_name = BaseState.find(i).name
 
 
+    a = Turn.new
+    a.pitcher_id = 4
+    a.batter_id = 8
+    @batter = T1batter.find_by id: a.batter_id
+    @pitcher = T2pitcher.find_by id: a.pitcher_id
+    a.save
   end
 
   def edit
@@ -81,6 +104,10 @@ class TurnsController < ApplicationController
     # base state image
     i = @turn.ending_base_state_id || 1
     @base_state_image = BaseState.find(i).image
+
+
+
+
 
   end
 
